758acd3e55fd0cae5732f4c525ca673dbc2e0407
antuncrnja <antuncrnja@gmail.com>
2021-09-16 21:54:56 +0200

    gsap update

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,22 @@
-import tripleMe from './components/tripleMe'
 import '../scss/style.scss'
+import tripleMe from './components/tripleMe'
+
+import { gsap } from 'gsap'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.timeline({
+	scrollTrigger: {
+	 trigger: '.card',
+	  start: 'top bottom',
+	  //end: 'top',
+	  scrub: true,
+	  pin: false,
+	  markers: true
+	}
+  })
+  .to('.card', {scale: 0.95, rotate: '3deg'})
+
 
 //Intersection Observer
 	const e = document.querySelectorAll('.fadein');
